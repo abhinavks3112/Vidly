5aaba9d4d42f94e491fcbdf77515d412160505a0
abhinavks3112 <abhiks3112@gmail.com>
2019-01-31 20:20:23 +0530

    Add project files.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    [RoutePrefix("Customers")]
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Customers
+        [Route("")]
+        public ActionResult Index()
+        {
+            var customers = _context.Customers;//Deferred Execution
+            return View(customers);
+        }
+
+        [Route("Details/{id}")]
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(x => x.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {Id=1, Name="Roger"},
+                new Customer {Id=2, Name="Drake"}
+            };
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    [RoutePrefix("Movies")]
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        [Route("")]
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        [Route("Details/{id}")]
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(x => x.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id=1, Name="Shrek"},
+                new Movie{Id=2, Name="Wall-e"}
+            };            
+        }
+
+    }
+}
